4aa7435fbfebcf8781c12de346bd1740fe91d152
Thomas Clauzel <t.clauzel@gmail.com>
2020-04-07 17:32:19 +0200

    Update GLPIhunter.sh

--- GLPIhunter.sh
+++ GLPIhunter.sh
@@ -25,8 +25,10 @@ rm ssltmp.txt > /dev/null 2>&1
 
 cat glpi.txt |  while read output
 do
+    echo "check for $output"
     nslookup $output > nslookuptmp.txt
     domain=$(cat nslookuptmp.txt |awk '/name/{print $4}' | sed 's/.$//')
+    echo "check for $domain"
     curl -k -IL https://$domain  --max-time 0,5 > ssltest.txt  2>&1 | grep "^HTTP\/"
     string0="HTTP/1.1 200 OK"
     if grep -qF "$string0" ssltest.txt;then
@@ -37,25 +39,25 @@ do
       var=$(grep glpi fqdn.txt)
       if [[ $var =~ glpi ]]
       then
-          curl -k https://$fqdn > curltmp.txt 2>&1
+          curl -k https://$fqdn --max-time 0,5 > curltmp.txt 2>&1
           string1="http://glpi-project.org/"
           if grep -qF "$string1" curltmp.txt;then
-            echo "FOUND ! GLPI : https://$fqdn"
+            echo "FOUND ! GLPI : https://$fqdn" >> glpi-server.txt
           fi
-          curl -k https://$fqdn/glpi/ > curltmp.txt 2>&1
+          curl -k https://$fqdn/glpi/ --max-time 0,5 > curltmp.txt 2>&1
           if grep -qF "$string1" curltmp.txt;then
-            echo "FOUND ! GLPI : https://$fqdn/glpi/"
+            echo "FOUND ! GLPI : https://$fqdn/glpi/" >> glpi-server.txt
           fi
       fi
     else
-      curl http://$domain > curltmp.txt 2>&1
+      curl -k http://$domain --max-time 0,5 > curltmp.txt 2>&1
       string1="http://glpi-project.org/"
       if grep -qF "$string1" curltmp.txt;then
-        echo "FOUND ! GLPI : http://$domain"
+        echo "FOUND ! GLPI : http://$domain" >> glpi-server.txt
       fi
-      curl http://$domain/glpi/ > curltmp.txt 2>&1
+      curl -k http://$domain/glpi/ --max-time 0,5 > curltmp.txt 2>&1
       if grep -qF "$string1" curltmp.txt;then
-        echo "FOUND ! GLPI : http://$domain/glpi/"
+        echo "FOUND ! GLPI : http://$domain/glpi/" >> glpi-server.txt
       fi
     fi
 done
